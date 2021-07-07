322e748888b97bbc8992fdd1838929bc446abc30
Hamza Al-Asal <hamzaalasa@gmail.com>
2021-07-07 23:44:18 +0300

    Build React Arc. for WriteIT

--- WriteIT/Startup.cs
+++ WriteIT/Startup.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 using WriteIT.Data;
 using WriteIT.Mappers;
 using WriteIT.Services;
@@ -16,6 +18,10 @@ namespace WriteIT
             services.AddAutoMapper(typeof(WriteITProfile));
             services.AddScoped<WriteITContext>();
             services.AddScoped<MovieService>();
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "clientapp/build";
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -30,9 +36,21 @@ namespace WriteIT
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                //endpoints.MapControllerRoute(
-                //we will describe the route after build the react js
-                //    );
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = Path.Join(env.ContentRootPath, "clientapp");
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
             });
         }
     }
